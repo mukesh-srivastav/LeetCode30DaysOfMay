60ed9e00b9c93c109f17d22cea22f2e39e73b1d9
Mukesh Srivastava <mukeshsrivastav691@gmail.com>
2020-05-27 00:39:19 +0530

    Day25 Uncrossed_Lines.java

---
+++ Day25_Uncrossed_Lines.java
@@ -0,0 +1,54 @@
+/**
+ * We write the integers of A and B (in the order they are given) on two separate horizontal lines.
+
+Now, we may draw connecting lines: a straight line connecting two numbers A[i] and B[j] such that:
+
+A[i] == B[j];
+The line we draw does not intersect any other connecting (non-horizontal) line.
+Note that a connecting lines cannot intersect even at the endpoints: each number can only belong to one connecting line.
+
+Return the maximum number of connecting lines we can draw in this way.
+
+ 
+
+Example 1:
+
+
+Input: A = [1,4,2], B = [1,2,4]
+Output: 2
+Explanation: We can draw 2 uncrossed lines as in the diagram.
+We cannot draw 3 uncrossed lines, because the line from A[1]=4 to B[2]=4 will intersect the line from A[2]=2 to B[1]=2.
+Example 2:
+
+Input: A = [2,5,1,2,5], B = [10,5,2,1,5,2]
+Output: 3
+ */
+public class Day25_Uncrossed_Lines {
+    public int maxUncrossedLines(int[] A, int[] B) {
+        int[][] dp = new int[A.length][B.length];
+        for(int i = 0; i < A.length; i++){
+            for(int j = 0; j < B.length; j++){
+                dp[i][j] = -1;
+            }
+        }
+        
+        return lcs(A, B, 0, 0, dp);
+    }
+    
+    
+    int lcs(int[] A, int[] B, int i, int j, int[][] dp){
+        if(i == A.length || j == B.length){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        if(A[i] == B[j]){
+            return dp[i][j] = 1 + lcs(A, B, i + 1, j + 1, dp);
+        }
+        else{
+            return dp[i][j] = Math.max(lcs(A, B, i + 1, j, dp), lcs(A, B, i, j + 1, dp));
+        }
+    }
+}
